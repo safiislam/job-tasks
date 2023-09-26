024a89d88f9edaf7d9aafa9e4dd7a9044412d068
safiislam <118913244+safiislam@users.noreply.github.com>
2023-09-26 20:04:32 +0600

    model done

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,37 +1,55 @@
 // import { Button, Modal } from 'bootstrap';
 import React, { useEffect, useState } from 'react';
-import Modal from './Modal/Modal';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 
 const Problem2 = (props) => {
-    const [datas, setDatas] = useState([])
+    const [datas, setDatas] = useState({})
+    const [datas1, setDatas1] = useState({})
     useEffect(() => {
         fetch('https://contact.mediusware.com/api/country-contacts/united%20states/',)
             .then(res => res.json())
             .then(data => setDatas(data))
     }, [])
-    console.log(datas)
-    const [isModalOpen, setModalOpen] = useState(false);
+    useEffect(() => {
+        fetch('https://contact.mediusware.com/api/contacts/',)
+            .then(res => res.json())
+            .then(data => setDatas1(data))
+    }, [])
+    console.log(datas?.results)
+    console.log(datas?.results?.map(item => item?.phone))
+    const [open, setOpen] = useState(false);
+    const [open1, setOpen1] = useState(false);
 
-    const handleOpenModal = () => {
-        setModalOpen(true);
-    };
-
-    const handleCloseModal = () => {
-        setModalOpen(false);
-    };
+    const onOpenModal = () => setOpen(true);
+    const onOpenModal1 = () => setOpen1(true);
+    const onCloseModal = () => setOpen(false);
+    const onCloseModal1 = () => setOpen1(false);
     return (
         <>
-            <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-                <h2>Modal Content</h2>
-                <p>This is the content of the modal.</p>
+            <Modal open={open} onClose={onCloseModal} center >
+                <h2>All contacts</h2>
+                <div>
+                    {
+                        datas1?.results?.map(item=><p>{item?.phone}</p>)
+                    }
+                </div>
+            </Modal>
+            <Modal open={open1} onClose={onCloseModal1} center>
+                <h2>All us contact</h2>
+                <div>
+                    {
+                        datas?.results?.map(item => <p>{item?.phone}</p>)
+                    }
+                </div>
             </Modal>
             <div className="container">
                 <div className="row justify-content-center mt-5">
                     <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
 
                     <div className="d-flex justify-content-center gap-3">
-                        <button className="btn btn-lg btn-outline-primary" type="button" onClick={handleOpenModal} >All Contacts</button>
-                        <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                        <button className="btn btn-lg btn-outline-primary" type="button" onClick={onOpenModal} >All Contacts</button>
+                        <button className="btn btn-lg btn-outline-warning" type="button" onClick={onOpenModal1} >US Contacts</button>
                     </div>
 
                 </div>
